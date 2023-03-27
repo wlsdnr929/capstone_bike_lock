b5be145e87b2cabb519cf7457bd88d2002726864
wlsdnr929 <wlsdnr929@naver.com>
2023-03-27 11:45:44 +0900

    액션바 적용

--- app/src/main/java/com/example/wifiwithkotlin/MainActivity.kt
+++ app/src/main/java/com/example/wifiwithkotlin/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.*
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.wifiwithkotlin.R
+import com.example.wifiwithkotlin.databinding.ActivityMainBinding
 import java.io.*
 import java.net.Socket
 import java.net.SocketTimeoutException
@@ -14,6 +15,9 @@ import java.net.UnknownHostException
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     private var mConnectionStatus: TextView? = null
     private var mInputEditText: EditText? = null
     private var mConversationArrayAdapter: ArrayAdapter<String>? = null
@@ -24,29 +28,51 @@ class MainActivity : AppCompatActivity() {
     private var mOut: PrintWriter? = null
     private var mIn: BufferedReader? = null
     private var mReceiverThread: Thread? = null
+    var flagLock:Boolean = false // 0: 잠김, 1: 열림
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         mConnectionStatus = findViewById<View>(R.id.connection_status_textview) as TextView
-        mInputEditText = findViewById<View>(R.id.input_string_edittext) as EditText
+        //mInputEditText = findViewById<View>(R.id.input_string_edittext) as EditText
         TAG = "TcpClient"
-        val mMessageListview = findViewById<View>(R.id.message_listview) as ListView
-        val sendButton = findViewById<View>(R.id.send_button) as Button
-        sendButton.setOnClickListener {
-            val sendMessage = mInputEditText!!.text.toString()
-            if (sendMessage.length > 0) {
-                if (!isConnected) showErrorDialog("서버로 접속된후 다시 해보세요.") else {
-                    Thread(SenderThread(sendMessage)).start()
-                    mInputEditText!!.setText(" ")
-                }
-            }
-        }
+        //val mMessageListview = findViewById<View>(R.id.message_listview) as ListView
+//        val sendButton = findViewById<View>(R.id.send_button) as Button
+//        sendButton.setOnClickListener {
+//            //val sendMessage = mInputEditText!!.text.toString()
+//            val sendMessage = flagLock.toString()
+//            if (sendMessage.length > 0) {
+//                if (!isConnected) showErrorDialog("서버로 접속된후 다시 해보세요.") else {
+//                    Thread(SenderThread(sendMessage)).start()
+//                    mInputEditText!!.setText(" ")
+//                }
+//            }
+//        }
         mConversationArrayAdapter = ArrayAdapter(
             this,
             android.R.layout.simple_spinner_dropdown_item
         )
-        mMessageListview.adapter = mConversationArrayAdapter
+        //mMessageListview.adapter = mConversationArrayAdapter
+        // 집
         Thread(ConnectThread("192.168.0.17", 8090)).start()
+        // 학교
+         //Thread(ConnectThread("192.168.0.55", 8090)).start()
+
+        binding.button.setOnClickListener {
+            if(flagLock){
+                binding.button.setImageResource(R.drawable.lock)
+                flagLock=false
+            }
+            else{
+                binding.button.setImageResource(R.drawable.unlock)
+                flagLock = true
+            }
+            var sendMessage = ""
+            if(flagLock == true) sendMessage = "1"
+            else sendMessage = "0"
+            Thread(SenderThread(sendMessage)).start()
+        }
     }
 
     override fun onDestroy() {
@@ -57,7 +83,7 @@ class MainActivity : AppCompatActivity() {
     override fun onBackPressed() {
         if (back_pressed + 2000 > System.currentTimeMillis()) {
             super.onBackPressed()
-            Log.d(TAG, "onBackPressed:")
+            Log.e(TAG, "onBackPressed:")
             isConnected = false
             finish()
         } else {
@@ -78,9 +104,9 @@ class MainActivity : AppCompatActivity() {
                 //mSocket.setSoTimeout(3000);
                 mServerIP = mSocket!!.remoteSocketAddress.toString()
             } catch (e: UnknownHostException) {
-                Log.d(TAG, "ConnectThread: can't find host")
+                Log.e(TAG, "ConnectThread: can't find host")
             } catch (e: SocketTimeoutException) {
-                Log.d(TAG, "ConnectThread: timeout")
+                Log.e(TAG, "ConnectThread: timeout")
             } catch (e: Exception) {
                 Log.e(TAG, "ConnectThread:" + e.message)
             }
@@ -101,16 +127,16 @@ class MainActivity : AppCompatActivity() {
             }
             runOnUiThread {
                 if (isConnected) {
-                    Log.d(TAG, "connected to $serverIP")
-                    mConnectionStatus!!.text = "connected to $serverIP"
+                    Log.e(TAG, "connected to $serverIP")
+                    mConnectionStatus!!.text = "$serverIP 연결 성공"
                     mReceiverThread = Thread(ReceiverThread())
                     mReceiverThread!!.start()
                 } else {
-                    Log.d(
+                    Log.e(
                         TAG,
                         "failed to connect to server $serverIP"
                     )
-                    mConnectionStatus!!.text = "failed to connect to server $serverIP"
+                    mConnectionStatus!!.text = "$serverIP 연결 실패"
                 }
             }
         }
@@ -125,9 +151,10 @@ class MainActivity : AppCompatActivity() {
             mOut!!.println(msg)
             mOut!!.flush()
             runOnUiThread {
-                Log.d(TAG, "send message: $msg")
+                Log.e(TAG, "send message: $msg")
                 //mConversationArrayAdapter!!.insert("Me - $msg", 0)
-                mConversationArrayAdapter!!.insert("<조이름: 매트릭스> 아두이노로 보내는 거: $msg", 0)
+
+                //mConversationArrayAdapter!!.insert("<조이름: 매트릭스> 아두이노로 보내는 거: $msg", 0)
             }
         }
     }
@@ -137,23 +164,22 @@ class MainActivity : AppCompatActivity() {
             try {
                 while (isConnected) {
                     if (mIn == null) {
-                        Log.d(TAG, "ReceiverThread: mIn is null")
+                        Log.e(TAG, "ReceiverThread: mIn is null")
                         break
                     }
                     val recvMessage = mIn!!.readLine()
                     if (recvMessage != null) {
                         runOnUiThread {
-                            Log.d(
-                                TAG,
-                                "recv message: $recvMessage"
-                            )
+
+                            Log.e(TAG,"recv message: $recvMessage")
                             // mConversationArrayAdapter!!.insert("$mServerIP - $recvMessage", 0)
-                            mConversationArrayAdapter!!.insert("<조이름: 매트릭스> 아두이노에서 받은 거:(바로 윗줄)", 0)
-                            mConversationArrayAdapter!!.insert("$recvMessage", 0)
+
+                            //mConversationArrayAdapter!!.insert("<조이름: 매트릭스> 아두이노에서 받은 거:(바로 윗줄)", 0)
+                            //mConversationArrayAdapter!!.insert("$recvMessage", 0)
                         }
                     }
                 }
-                Log.d(TAG, "ReceiverThread: thread has exited")
+                Log.e(TAG, "ReceiverThread: thread has exited")
                 if (mOut != null) {
                     mOut!!.flush()
                     mOut!!.close()
